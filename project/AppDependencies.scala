4a643577d566dc86405418d58b3f637d41bfdd87
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-04-16 12:13:50 +0100

    APIS-6956 - Play json lib to 3.x (#13)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,12 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "8.5.0"
-  val apiDomainVersion         = "0.11.0"
-  val commonDomainVersion      = "0.10.0"
+  val apiDomainVersion         = "0.16.0"
+  val commonDomainVersion      = "0.13.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "9.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"   % "9.5.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-30" % "1.10.0",
     "org.apache.commons" % "commons-csv"                  % "1.10.0",
     "uk.gov.hmrc"       %% "api-platform-api-domain"      % apiDomainVersion
