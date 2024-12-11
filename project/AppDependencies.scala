6e58b05f0d46eae2e3c88b5b0122ba990d8a8e00
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-12-11 17:12:46 +0000

    APIS-7388 Bump application domain library to latest version (#20)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ object AppDependencies {
 
   private val bootstrapVersion = "9.5.0"
   val apiDomainVersion         = "0.19.1"
-  val commonDomainVersion      = "0.17.0"
-  val appDomainVersion         = "0.62.0"
+  val commonDomainVersion      = "0.18.0"
+  val appDomainVersion         = "0.65.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"      % bootstrapVersion,
