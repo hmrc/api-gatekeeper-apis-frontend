6b612424b3e61ffb4e200cf834741db435883d61
kskourtis1 <kskourtis1@users.noreply.github.com>
2025-05-22 10:47:24 +0100

    APIS-7571: Remove old endpoint and unused code for approvals (#27)

--- app/uk/gov/hmrc/apigatekeeperapisfrontend/config/GatekeeperConfig.scala
+++ app/uk/gov/hmrc/apigatekeeperapisfrontend/config/GatekeeperConfig.scala
@@ -21,6 +21,6 @@ case class GatekeeperConfig(gatekeeperBaseUrl: String) {
   lazy val termsOfUsePageUri   = s"${gatekeeperBaseUrl}/api-gatekeeper-approvals/terms-of-use"
   lazy val developersPageUri   = s"${gatekeeperBaseUrl}/api-gatekeeper/developers"
   lazy val emailsPageUri       = s"${gatekeeperBaseUrl}/api-gatekeeper/emails"
-  lazy val pendingUri          = s"${gatekeeperBaseUrl}/api-gatekeeper/pending"
+  lazy val pendingUri          = s"${gatekeeperBaseUrl}/api-gatekeeper/api-approvals"
   lazy val xmlUri              = s"${gatekeeperBaseUrl}/api-gatekeeper/xml-organisations"
 }
