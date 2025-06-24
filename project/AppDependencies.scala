e66ef52b092e53c69b5e34b9652f958221deea4e
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-06-24 09:19:19 +0100

    APIS-7644 - Update for rebranding work (#28)
    * APIS-7644 - Update for rebranding work
    
    * APIS-7644 - PR Bot warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.13.0"
   val apiDomainVersion         = "0.19.1"
   val commonDomainVersion      = "0.18.0"
-  val appDomainVersion         = "0.75.0"
+  val appDomainVersion         = "0.79.0"
 
+  private val playfrontendVersion = "12.6.0"
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"      % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"      % "11.12.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"      % playfrontendVersion,
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "3.1.0",
     "org.apache.commons" % "commons-csv"                     % "1.10.0",
     "uk.gov.hmrc"       %% "api-platform-api-domain"         % apiDomainVersion,
