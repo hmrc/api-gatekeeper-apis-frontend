7539f36f6f4c4e4548a72c98edaaf3592ff00e10
John Green <john.green@digital.hmrc.gov.uk>
2026-03-23 12:50:15 +0000

    API-9020: Bump api-domain and app-domain and evergreen

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion    = "10.5.0"
-  private val apiDomainVersion    = "0.20.0"
-  private val appDomainVersion    = "0.95.0"
-  private val playfrontendVersion = "12.26.0"
+  private val bootstrapVersion    = "10.7.0"
+  private val apiDomainVersion    = "1.0.0"
+  private val appDomainVersion    = "1.0.0"
+  private val playfrontendVersion = "12.32.0"
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"      % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"      % playfrontendVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "3.1.0",
-    "org.apache.commons" % "commons-csv"                     % "1.10.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "4.3.0",
+    "org.apache.commons" % "commons-csv"                     % "1.14.1",
     "uk.gov.hmrc"       %% "api-platform-api-domain"         % apiDomainVersion,
     "uk.gov.hmrc"       %% "api-platform-application-domain" % appDomainVersion
   )
 
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30"                   % bootstrapVersion,
-    "org.jsoup"    % "jsoup"                                    % "1.17.2",
+    "org.jsoup"    % "jsoup"                                    % "1.21.1",
     "uk.gov.hmrc" %% "api-platform-application-domain-fixtures" % appDomainVersion
   ).map(_ % "test")
 }
