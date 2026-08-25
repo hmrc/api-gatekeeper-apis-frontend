e60765d962b3e39164c34b492938ff5e94de9d45
Pete Slater <28756214+peteslater-ee@users.noreply.github.com>
2026-08-25 13:22:15 +0100

    API-9360 - Upgrading libs (#47)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,9 @@ object AppDependencies {
 
   private val bootstrapVersion    = "10.8.0"
 
-  private val apiDomainVersion    = "1.4.0"
-  private val appDomainVersion    = "1.4.0"
+  private val commonDomainVersion = "1.4.0"
+  private val apiDomainVersion    = "1.8.0"
+  private val appDomainVersion    = "1.6.0"
   private val playfrontendVersion = "13.11.0"
 
 
@@ -14,6 +15,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"      % playfrontendVersion,
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "4.4.0",
     "org.apache.commons" % "commons-csv"                     % "1.14.1",
+    "uk.gov.hmrc"       %% "api-platform-common-domain"      % commonDomainVersion,
     "uk.gov.hmrc"       %% "api-platform-api-domain"         % apiDomainVersion,
     "uk.gov.hmrc"       %% "api-platform-application-domain" % appDomainVersion
   )
@@ -21,6 +23,7 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30"                   % bootstrapVersion,
     "org.jsoup"    % "jsoup"                                    % "1.21.1",
-    "uk.gov.hmrc" %% "api-platform-application-domain-fixtures" % appDomainVersion
+    "uk.gov.hmrc" %% "api-platform-application-domain-fixtures" % appDomainVersion,
+    "uk.gov.hmrc" %% "api-platform-common-domain-fixtures"      % commonDomainVersion
   ).map(_ % "test")
 }
